3f4ad65cb0f103dfd62e0530cb319c67ff76dec3
admin <rong@agile-solutions.ch>
2026-02-09 17:37:12 +0100

    add azure devops pipelines

--- account-service/src/test/java/com/agilesolutions/account/repository/AccountRepositoryTest.java
+++ account-service/src/test/java/com/agilesolutions/account/repository/AccountRepositoryTest.java
@@ -2,6 +2,7 @@ package com.agilesolutions.account.repository;
 
 import com.agilesolutions.account.entity.Account;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.data.mongo.DataMongoTest;
@@ -11,9 +12,6 @@ import org.springframework.test.context.ContextConfiguration;
 import java.time.LocalDate;
 import java.util.List;
 
-@DataMongoTest
-@ContextConfiguration(classes = {AccountRepository.class})
-@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 class AccountRepositoryTest extends BaseMongoDBIntegrationTest {
 
 
--- account-service/src/test/java/com/agilesolutions/account/repository/BaseMongoDBIntegrationTest.java
+++ account-service/src/test/java/com/agilesolutions/account/repository/BaseMongoDBIntegrationTest.java
@@ -2,10 +2,14 @@ package com.agilesolutions.account.repository;
 
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.testcontainers.containers.MongoDBContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
+@Testcontainers
 public class BaseMongoDBIntegrationTest {
 
+    @Container
     @ServiceConnection
     public static MongoDBContainer mongoDBContainer = new MongoDBContainer(DockerImageName.parse("mongo:4.0.10"));
 
