3b1385418133fb7e7ad9947381f878c8e1fb7ee2
admin <rong@agile-solutions.ch>
2026-02-10 11:10:45 +0100

    add PG test containers for integration testing

--- client-service/src/test/java/com/agilesolutions/client/repository/ClientRepositoryTest.java
+++ client-service/src/test/java/com/agilesolutions/client/repository/ClientRepositoryTest.java
@@ -3,9 +3,13 @@ package com.agilesolutions.client.repository;
 import com.agilesolutions.client.entity.Client;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.r2dbc.DataR2dbcTest;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import reactor.core.publisher.Mono;
 import reactor.test.StepVerifier;
 
+@DataR2dbcTest
+@AutoConfigureTestDatabase(replace= AutoConfigureTestDatabase.Replace.NONE)
 class ClientRepositoryTest extends BasePGIntegrationTest {
 
     @Autowired
