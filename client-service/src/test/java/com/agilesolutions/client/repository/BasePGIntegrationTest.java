0f592b2c6ff40eac0b27d9331c356cc9b07575f7
admin <rong@agile-solutions.ch>
2026-02-10 11:42:06 +0100

    add PG test containers for integration testing

--- client-service/src/test/java/com/agilesolutions/client/repository/BasePGIntegrationTest.java
+++ client-service/src/test/java/com/agilesolutions/client/repository/BasePGIntegrationTest.java
@@ -7,7 +7,7 @@ import org.testcontainers.junit.jupiter.Container;
 public class BasePGIntegrationTest {
 
     @Container
-    @ServiceConnection
+    @ServiceConnection // Automatically handles R2DBC URL/User/Pass
     protected static final PostgreSQLContainer<?> dbContainer = new PostgreSQLContainer<>("postgres:16-alpine");
 
 }
