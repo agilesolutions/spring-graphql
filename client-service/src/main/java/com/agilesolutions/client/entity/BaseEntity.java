7536307a6d17dca96707fa0c2db8c06e04571d89
admin <rong@agile-solutions.ch>
2026-02-11 09:48:08 +0100

    add PG test containers for integration testing

--- client-service/src/main/java/com/agilesolutions/client/entity/BaseEntity.java
+++ client-service/src/main/java/com/agilesolutions/client/entity/BaseEntity.java
@@ -1,7 +1,6 @@
 package com.agilesolutions.client.entity;
 
 import jakarta.persistence.Column;
-import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,12 +9,10 @@ import org.springframework.data.annotation.CreatedBy;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
 
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
 @Getter
 @Setter
 @ToString
