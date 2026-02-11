06b386773248e177b8a27726beae24ace8b7ad64
admin <rong@agile-solutions.ch>
2026-02-11 11:10:47 +0100

    add PG test containers for integration testing

--- client-service/src/test/java/com/agilesolutions/client/repository/TestcontainersConfiguration.java
+++ client-service/src/test/java/com/agilesolutions/client/repository/TestcontainersConfiguration.java
@@ -3,15 +3,11 @@ package com.agilesolutions.client.repository;
 import io.r2dbc.spi.ConnectionFactory;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
 import org.springframework.data.r2dbc.repository.config.EnableR2dbcRepositories;
-import org.springframework.r2dbc.connection.init.CompositeDatabasePopulator;
-import org.springframework.r2dbc.connection.init.ConnectionFactoryInitializer;
-import org.springframework.r2dbc.connection.init.ResourceDatabasePopulator;
 
 @TestConfiguration(proxyBeanMethods = false)
-@EnableR2dbcRepositories
+//@EnableR2dbcRepositories
 class TestcontainersConfiguration {
 
     /**
